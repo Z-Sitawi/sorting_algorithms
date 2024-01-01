81420a2b7e67d4ec37a5ee067f5e33f67149286b
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-01-01 08:55:50 -0800

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL)
 		return;
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; i++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
