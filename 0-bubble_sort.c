e92c3bafcaf644d00a0adf1e1092c155e5640208
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-01-01 06:19:48 -0800

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts array of ints in ascendingly using Bubble sort algorithm
+ *
+ * @array: the array to sort.
+ * @size: The lenght of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	printf("not done yet");
+}
