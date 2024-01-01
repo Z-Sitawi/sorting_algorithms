afd0f3747a364fce645227bf0e72f0b1f8dbba50
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-01-01 08:09:43 -0800

    task 0

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H_
+#define _SORT_H_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -27,4 +27,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif /* SOART_H */
+#endif /* _SOART_H_ */
