b52d718042b30be4af9e3b37d489b8159ecf3fe7
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-01-01 09:24:19 -0800

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts array in ascendingly using Selection sort algorithm
+ * @array: A Pointer to the array to be printed
+ * @size: Number of elements in the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, idx;
+	int min;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				idx = j;
+			}
+		}
+		if (idx != i)
+		{
+			array[idx] = array[i];
+			array[i] = min;
+			print_array(array, size);
+		}
+	}
+}
